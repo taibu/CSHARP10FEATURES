5a1eb8b5a8177cfaf341a70c3d160d30022b2b82
taibu <isabiryetai645@gmail.com>
2022-05-04 11:24:43 +0300

    With filescope namespaces, you do not have to wrap all the classes in a file. Just add the namespace in a statement at the top and it will apply to all clasess in the file

---
+++ FileScopeNameSpaces/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CSHARP10FEATURES.FileScopeNameSpaces;
+
+public class Animal
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
+
+public class GoaT:Animal
+{
+    public string Goattype { get; set; }
+    
+}
